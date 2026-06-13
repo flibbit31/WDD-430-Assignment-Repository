16586102551a7daa9f749774802f953c91ead1cf
flibbit31 <flibbit31@yahoo.com>
2026-06-13 13:39:58 -0600

    Finished W08 Assignment

--- cms/src/app/app-module.ts
+++ cms/src/app/app-module.ts
@@ -1,5 +1,6 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { App } from './app';
@@ -38,7 +39,7 @@ import { ContactEditComponent } from './contacts/contact-edit/contact-edit.compo
     DocumentEditComponent,
     ContactEditComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- cms/src/app/contacts/contact-edit/contact-edit.component.ts
+++ cms/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-edit',
@@ -6,4 +11,57 @@ import { Component } from '@angular/core';
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css',
 })
-export class ContactEditComponent {}
+export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
+
+  constructor(private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        const id = params['id'];
+        if (id === undefined || id === null) {
+          this.editMode = false;
+          return;
+        }
+
+        this.originalContact = this.contactService.getContact(id);
+        if (this.originalContact === undefined || this.originalContact === null) {
+          return;
+        }
+
+        this.editMode = true;
+        this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+        if (this.originalContact.group !== undefined && this.originalContact.group !== null) {
+          this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
+        }
+      }
+    )
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newContact = new Contact(value.id, value.name, value.email, value. phone, value.imageUrl, null);
+
+    if (this.editMode === true) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    }
+    else {
+      this.contactService.addContact(newContact);
+    }
+
+    this.router.navigate(['/contacts']);
+  }
+
+  onCancel() {
+    this.router.navigate(['/contacts']);
+  }
+}
